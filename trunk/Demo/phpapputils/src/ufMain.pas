ee2601aacb7a780f29a512416637dcb5bac5fb20
Darcy.Gong@gmail.com <Darcy.Gong@gmail.com@0607d5f9-6615-d192-5873-489cbdfef8a4>
2011-12-03 04:27:08 +0000

    Demo for Result Array

--- trunk/Demo/phpapputils/src/ufMain.pas
+++ trunk/Demo/phpapputils/src/ufMain.pas
@@ -14,6 +14,7 @@ type
   private
     procedure _logger_add;
     procedure _get_global_var;
+    procedure _get_demo_array;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Refresh; override;
@@ -32,10 +33,14 @@ type
     btnRunPhpInfo: TButton;
     mmoLog: TMemo;
     btnGetGlobalVariants: TButton;
+    btnReturnArray: TButton;
+    btnGetKeyArray: TButton;
     procedure btnRunPhpInfoClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnGetGlobalVariantsClick(Sender: TObject);
+    procedure btnReturnArrayClick(Sender: TObject);
+    procedure btnGetKeyArrayClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -61,6 +66,27 @@ end;
 procedure TfrmMain.btnGetGlobalVariantsClick(Sender: TObject);
 begin
   phpPSV.RunCode('get_global_var(array("a"=>"ZZZ","b"=>"YYY","c"=>"XXX","d"=>"WWW"));');
+end;
+
+procedure TfrmMain.btnGetKeyArrayClick(Sender: TObject);
+var
+  html:string;
+begin
+  //phpPSV.PreVars.Add('__PHPFILE__=gettask.php');
+  //phpPSV.Codes.Add('if (file_exists($__PHPFILE__)) { include($__PHPFILE__); } ');
+  phpPSV.Codes.Add('echo "<pre>";print_r(get_demo_array());echo "</pre>";');
+  html := phpPSV.RunPreCode;
+  webkitView.Browser.MainFrame.LoadString(html,'http://localphpapp/demo/getkeyarray');
+  pgcMain.ActivePage := tsWebView;
+end;
+
+procedure TfrmMain.btnReturnArrayClick(Sender: TObject);
+var
+  html:string;
+begin
+  html := phpPSV.RunCode('phpinfo();');
+  webkitView.Browser.MainFrame.LoadString(html,'http://localphpapp/demo/phpinfo');
+  pgcMain.ActivePage := tsWebView;
 end;
 
 procedure TfrmMain.btnRunPhpInfoClick(Sender: TObject);
@@ -101,8 +127,37 @@ begin
   Functions.Clear;
   RegisterMethod('logger_add','Add Log to Host: logger_add($text);',
     _logger_add, [tpString]);
-  RegisterMethod('get_global_var','Get Php Global var: get_global_var();',
+  RegisterMethod('get_global_var','Get Php Global var: get_global_var($array);',
     _get_global_var, [tpArray]);
+  RegisterMethod('get_demo_array','Get demo array: get_demo_array();',
+    _get_demo_array, []);
+end;
+
+procedure TPhpDemoLib._get_demo_array;
+var
+  pval:pzval;
+  zendvar:TZendVariable;
+begin
+  LOGADD('_get_demo_array start');
+  //serv := MAKE_STD_ZVAL;
+  //add_assoc_string_ex(serv,'user',SizeOf('user'),'1234567890',0);
+{
+  zendvar := GetReturnOutputArgZendVar();
+  pval := zendvar.AsZendVariable;
+  if _array_init(pval, nil, 0) <> FAILURE then
+  begin
+    add_assoc_string_ex(pval,'user',SizeOf('user')+1,'1234567890',1);
+    add_next_index_string(pval,'0000000000',1);
+    add_assoc_string_ex(pval,'user1',SizeOf('user1')+1,'0987654321',1);
+  end;
+}
+  RetArray.Add('abc=123456');
+  RetArray.Add('234561');
+  RetArray.Add('abc1=123456');
+  RetArray.Add('abc=555555');
+  ReturnOutputArray(RetArray);
+  //zendvar.AsString := '1234567890';
+  LOGADD('_get_demo_array end');
 end;
 
 procedure TPhpDemoLib._get_global_var;
